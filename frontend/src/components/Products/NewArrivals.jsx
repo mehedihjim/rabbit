52cdceecb31fe6c2e112473d2d325437aa82b8ac
Mehedi H Jim <mhjim.info@gmail.com>
2025-03-06 15:32:28 +0600

    New Arrival Done

--- frontend/src/components/Products/NewArrivals.jsx
+++ frontend/src/components/Products/NewArrivals.jsx
@@ -1,137 +1,224 @@
-import React from 'react'
-import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
-import { Link } from 'react-router'
+import React, { useEffect, useRef, useState } from "react";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router";
 
 const NewArrivals = () => {
+  const scrollRef = useRef(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(false);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
 
-    const newArrivals = [
+  const newArrivals = [
+    {
+      _id: "1",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
         {
-            _id: "1",
-            name: "Stylish Jacket",
-            price: 120,
-            images: [
-                {
-                    url: "https://picsum.photos/500/500?random=1",
-                    altText: "Stylish Jacket",
-                }
-            ]
+          url: "https://picsum.photos/500/500?random=1",
+          altText: "Stylish Jacket",
         },
+      ],
+    },
+    {
+      _id: "2",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
         {
-            _id: "2",
-            name: "Stylish Jacket",
-            price: 120,
-            images: [
-                {
-                    url: "https://picsum.photos/500/500?random=2",
-                    altText: "Stylish Jacket",
-                }
-            ]
+          url: "https://picsum.photos/500/500?random=2",
+          altText: "Stylish Jacket",
         },
+      ],
+    },
+    {
+      _id: "3",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
         {
-            _id: "3",
-            name: "Stylish Jacket",
-            price: 120,
-            images: [
-                {
-                    url: "https://picsum.photos/500/500?random=3",
-                    altText: "Stylish Jacket",
-                }
-            ]
+          url: "https://picsum.photos/500/500?random=3",
+          altText: "Stylish Jacket",
         },
+      ],
+    },
+    {
+      _id: "4",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
         {
-            _id: "4",
-            name: "Stylish Jacket",
-            price: 120,
-            images: [
-                {
-                    url: "https://picsum.photos/500/500?random=4",
-                    altText: "Stylish Jacket",
-                }
-            ]
+          url: "https://picsum.photos/500/500?random=4",
+          altText: "Stylish Jacket",
         },
+      ],
+    },
+    {
+      _id: "5",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
         {
-            _id: "5",
-            name: "Stylish Jacket",
-            price: 120,
-            images: [
-                {
-                    url: "https://picsum.photos/500/500?random=5",
-                    altText: "Stylish Jacket",
-                }
-            ]
+          url: "https://picsum.photos/500/500?random=5",
+          altText: "Stylish Jacket",
         },
+      ],
+    },
+    {
+      _id: "6",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
         {
-            _id: "6",
-            name: "Stylish Jacket",
-            price: 120,
-            images: [
-                {
-                    url: "https://picsum.photos/500/500?random=6",
-                    altText: "Stylish Jacket",
-                }
-            ]
+          url: "https://picsum.photos/500/500?random=6",
+          altText: "Stylish Jacket",
         },
+      ],
+    },
+    {
+      _id: "7",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
         {
-            _id: "7",
-            name: "Stylish Jacket",
-            price: 120,
-            images: [
-                {
-                    url: "https://picsum.photos/500/500?random=7",
-                    altText: "Stylish Jacket",
-                }
-            ]
+          url: "https://picsum.photos/500/500?random=7",
+          altText: "Stylish Jacket",
         },
+      ],
+    },
+    {
+      _id: "8",
+      name: "Stylish Jacket",
+      price: 120,
+      images: [
         {
-            _id: "8",
-            name: "Stylish Jacket",
-            price: 120,
-            images: [
-                {
-                    url: "https://picsum.photos/500/500?random=8",
-                    altText: "Stylish Jacket",
-                }
-            ]
+          url: "https://picsum.photos/500/500?random=8",
+          altText: "Stylish Jacket",
         },
-    ]
+      ],
+    },
+  ];
 
-    return (
-        <section>
-            {/* Section Heading */}
-            <div className="container mx-auto text-center mb-10 relative">
-                <h2 className='text-3xl font-bold mb-4'>
-                    Explore New Arrivals
-                </h2>
-                <p className='text-lg text-gray-600 mb-8'>
-                    Discover the latest styles straight off the runway, freshly added to keep your wardrobe on the cutting edge of fashion.
-                </p>
-                {/* Slide Buttons */}
-                <div className="absolute right-0 bottom-[-30px] flex space-x-2">
-                    <button className='p-2 border border-gray-300 bg-white text-black rounded-full'>
-                        <FiChevronLeft className='cursor-pointer text-2xl' />
-                    </button>
-                    <button className='p-2 border border-gray-300 bg-white text-black rounded-full'>
-                        <FiChevronRight className='cursor-pointer text-2xl' />
-                    </button>
-                </div>
+  //   =======Mouse Directions
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setStartX(e.pageX - scrollRef.current.offsetLeft);
+    setScrollLeft(scrollRef.current.scrollLeft);
+  };
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return;
+    const x = e.pageX - scrollRef.current.offsetLeft;
+    const walk = x - startX;
+    scrollRef.current.scrollLeft = scrollLeft - walk;
+  };
+
+  const handleMouseUporLeave = (e) => {
+    setIsDragging(false);
+  };
+
+  const scroll = (direction) => {
+    const scrollAmount = direction === "left" ? -300 : 300;
+
+    scrollRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  };
+
+  //   ======update scroll buttons=========
+  const updateScrollButtons = () => {
+    const container = scrollRef.current;
+
+    if (container) {
+      const leftScroll = container.scrollLeft;
+      const rightScrollable =
+        container.scrollWidth > leftScroll + container.clientWidth;
+
+      setCanScrollLeft(leftScroll > 0);
+      setCanScrollRight(rightScrollable);
+    }
+
+    console.log({
+      scrollLeft: container.scrollLeft,
+      clientWidth: container.clientWidth,
+      containerScrollWidth: container.scrollWitdth,
+      offsetLeft: scrollRef.current.offsetLeft,
+    });
+  };
+
+  useEffect(() => {
+    const container = scrollRef.current;
+    if (container) {
+      container.addEventListener("scroll", updateScrollButtons);
+      updateScrollButtons();
+      return () => container.removeEventListener("scroll", updateScrollButtons);
+    }
+  }, []);
+
+  return (
+    <section className="py-16 px-4 lg:px-0">
+      {/* ===========Section Heading=========== */}
+      <div className="container mx-auto text-center mb-10 relative">
+        <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
+        <p className="text-lg text-gray-600 mb-8">
+          Discover the latest styles straight off the runway, freshly added to
+          keep your wardrobe on the cutting edge of fashion.
+        </p>
+        {/* ==========Slide Buttons========== */}
+        <div className="absolute right-0 bottom-[-30px] flex space-x-2">
+          <button
+            onClick={() => scroll("left")}
+            disabled={!canScrollLeft}
+            className={`p-2 border border-gray-300 ${
+              canScrollLeft ? "bg-white text-black" : "text-gray-300"
+            } rounded-full`}
+          >
+            <FiChevronLeft className="cursor-pointer text-2xl" />
+          </button>
+          <button
+            onClick={() => scroll("right")}
+            disabled={!canScrollRight}
+            className={`p-2 border border-gray-300 ${
+              canScrollRight ? "bg-white text-black" : "text-gray-300"
+            } rounded-full`}
+          >
+            <FiChevronRight className="cursor-pointer text-2xl" />
+          </button>
+        </div>
+      </div>
+      {/* Product slider */}
+      <div
+        ref={scrollRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUporLeave}
+        onMouseLeave={handleMouseUporLeave}
+        className={`container mx-auto overflow-x-scroll hide-scrollbar flex space-x-6 relative ${
+          isDragging ? "cursor-grabbing" : "cursor-grab"
+        }`}
+      >
+        {newArrivals.map((product) => (
+          <div
+            key={product._id}
+            className="min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative"
+          >
+            <img
+              src={product.images[0]?.url}
+              alt={product.images[0]?.altText || product.name}
+              className="w-full h-[500px] object-cover rounded-lg"
+              draggable="false"
+            />
+            <div className="absolute bottom-4 left-4 right-0 bg-opacity-50 backdrop-blur-md w-[60%] text-white p-4 ">
+              <Link to={`/product/${product._id}`} className="block">
+                <h4 className="font-medium underline">{product.name}</h4>
+                <p className="mt-1 $">${product.price}</p>
+              </Link>
             </div>
-            {/* Product slider */}
-            <div className="container mx-auto overflow-x-scroll flex space-x-6 relative">
-                {newArrivals.map((product) => (
-                    <div key={product._id} className="min-w-[100%] sm:min-w-[50%] lg:min-w-[30%] relative">
-                        <img src={product.images[0]?.url} alt={product.images[0]?.altText || product.name} className='w-full h-[500px] object-cover rounded-lg' />
-                        <div className="absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg">
-                            <Link to={`/product/${product._id}`} className='block'>
-                                <h4 className='font-medium'>{product.name}</h4>
-                                <p className='mt-1 $'>${product.price}</p>
-                            </Link>
-                        </div>
-                    </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-                ))}
-            </div>
-        </section>
-    )
-}
-
-export default NewArrivals
+export default NewArrivals;
