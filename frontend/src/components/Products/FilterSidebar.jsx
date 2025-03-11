933c0a171eae66108192e45263c30ac9605f7805
Mehedi H Jim <mhjim.info@gmail.com>
2025-03-12 00:36:55 +0600

    Filter Functions Completed

--- frontend/src/components/Products/FilterSidebar.jsx
+++ frontend/src/components/Products/FilterSidebar.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router";
+import { useNavigate, useSearchParams } from "react-router";
 
 const FilterSidebar = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [filter, setFilters] = useState({
+  const navigate = useNavigate();
+  const [filters, setFilters] = useState({
     category: "",
     gender: "",
     color: "",
@@ -63,13 +64,69 @@ const FilterSidebar = () => {
       gender: params.gender || "",
       color: params.color || "",
       size: params.size ? params.size.split(",") : [],
-      material: params.materaial ? params.materaial.split(",") : [],
+      material: params.material ? params.material.split(",") : [],
       brand: params.brand ? params.brand.split(",") : [],
       minPrice: params.minPrice || 0,
       maxPrice: params.maxPrice || 100,
     });
     setPriceRange([0, params.maxPrice || 100]);
   }, [searchParams]);
+
+  // const handleFilterChange = (e) => {
+  //   const { name, value, checked, type } = e.target;
+  //   const newFilters = { ...filters };
+
+  //   if (type === "checked") {
+  //     if (checked) {
+  //       newFilters[name] = [...(newFilters[name] || []), value];
+  //     } else {
+  //       newFilters[name] = newFilters[name].filter((item) => item !== value);
+  //     }
+  //   } else {
+  //     newFilters[name] = value;
+  //   }
+  //   setFilters(newFilters);
+  //   console.log(newFilters);
+  // };
+
+  const handleFilterChange = (e) => {
+    const { name, value, checked, type } = e.target;
+    const newFilters = { ...filters };
+
+    if (type === "checkbox") {
+      if (checked) {
+        newFilters[name] = [...newFilters[name], value];
+      } else {
+        newFilters[name] = newFilters[name].filter((item) => item !== value);
+      }
+    } else {
+      newFilters[name] = value;
+    }
+
+    setFilters(newFilters);
+    updateURLParams(newFilters);
+  };
+
+  const updateURLParams = (newFilters) => {
+    const params = new URLSearchParams();
+    Object.keys(newFilters).forEach((key) => {
+      if (Array.isArray(newFilters[key]) && newFilters[key].length > 0) {
+        params.append(key, newFilters[key].join(","));
+      } else if (newFilters[key]) {
+        params.append(key, newFilters[key]);
+      }
+    });
+    setSearchParams(params);
+    navigate(`?${params.toString()}`);
+  };
+
+  const handlePriceChange = (e) => {
+    const newPrice = e.target.value;
+    setPriceRange([0, newPrice]);
+    const newFilters = { ...filters, minPrice: 0, maxPrice: newPrice };
+    setFilters(filters);
+    updateURLParams(newFilters);
+  };
 
   return (
     <div className="p-4">
@@ -83,6 +140,9 @@ const FilterSidebar = () => {
               type="radio"
               name="category"
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border border-gray-300"
+              value={category}
+              onChange={handleFilterChange}
+              checked={filters.category === category}
             />
             <span className="text-gray-700">{category}</span>
           </div>
@@ -95,8 +155,11 @@ const FilterSidebar = () => {
           <div className="flex items-center mb-1" key={gender}>
             <input
               type="radio"
-              name="category"
+              name="gender"
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border border-gray-300"
+              value={gender}
+              onChange={handleFilterChange}
+              checked={filters.gender === gender}
             />
             <span className="text-gray-700">{gender}</span>
           </div>
@@ -113,7 +176,11 @@ const FilterSidebar = () => {
             <button
               key={color}
               name="color"
-              className="w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105"
+              value={color}
+              onClick={handleFilterChange}
+              className={`w-8 h-8 rounded-full border border-gray-300 cursor-pointer transition hover:scale-105 ${
+                filters.color === color ? "ring-2 ring-blue-500" : ""
+              }`}
               style={{ backgroundColor: color.toLowerCase() }}
             ></button>
           ))}
@@ -131,6 +198,9 @@ const FilterSidebar = () => {
               type="checkbox"
               name="size"
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border border-gray-300"
+              value={size}
+              onChange={handleFilterChange}
+              checked={filters.size.includes(size)}
             />
             <span className="text-gray-700">{size}</span>
           </div>
@@ -146,13 +216,16 @@ const FilterSidebar = () => {
             <input
               type="checkbox"
               name="size"
+              value={material}
+              onChange={handleFilterChange}
+              checked={filters.material.includes(material)}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border border-gray-300"
             />
             <span className="text-gray-700">{material}</span>
           </div>
         ))}
       </div>
-      {/* Material Filter */}
+      {/* Brands Filter */}
       <div className="mb-6">
         <label htmlFor="" className="block text-gray-600 font-medium mb-2">
           Brands
@@ -162,6 +235,9 @@ const FilterSidebar = () => {
             <input
               type="checkbox"
               name="size"
+              value={brand}
+              onChange={handleFilterChange}
+              checked={filters.brand.includes(brand)}
               className="mr-2 h-4 w-4 text-blue-500 focus:ring-blue-400 border border-gray-300"
             />
             <span className="text-gray-700">{brand}</span>
@@ -176,6 +252,8 @@ const FilterSidebar = () => {
         </label>
         <input
           type="range"
+          value={priceRange[1]}
+          onChange={handlePriceChange}
           name="priceRange"
           min={0}
           max={100}
