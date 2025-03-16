fd1888c6df25d94b8747189e3fe0a3fb40e38ad0
Mehedi H Jim <mhjim.info@gmail.com>
2025-03-16 15:01:48 +0600

    Admin Dashboard Design Complete

--- frontend/src/components/Admin/OrderManagement.jsx
+++ frontend/src/components/Admin/OrderManagement.jsx
@@ -1,7 +1,85 @@
 import React from "react";
 
 const OrderManagement = () => {
-  return <div>OrderManagement</div>;
+  const orders = [
+    {
+      _id: 12312321,
+      user: {
+        name: "Hatake Kakashi",
+      },
+      totalPrice: 110,
+      status: "Processing",
+    },
+  ];
+
+  const handleStatusChange = (orderId, status) => {
+    console.log({ id: orderId, status });
+  };
+
+  return (
+    <div className="max-w-7xl mx-auto p-6">
+      <h2 className="text-2xl font-bold mb-6">Order Management</h2>
+      <div className="overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="min-w-full text-left text-gray-500">
+          <thead className="bg-gray-100 text-xs uppercase text-gray-700">
+            <tr>
+              <th className="py-3 px-4">Order ID</th>
+              <th className="py-3 px-4">Customer</th>
+              <th className="py-3 px-4">Total Price</th>
+              <th className="py-3 px-4">Status</th>
+              <th className="py-3 px-4">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.length > 0 ? (
+              orders.map((order) => (
+                <tr
+                  key={order._id}
+                  className="border-b border-gray-300 hover:bg-gray-50 cursor-pointer"
+                >
+                  <td className="px-4 py-4 font-medium text-gray-900 whitespace-nowrap">
+                    #{order._id}
+                  </td>
+                  <td className="p-4">{order.user.name}</td>
+                  <td className="p-4">${order.totalPrice}</td>
+                  <td className="p-4">
+                    <select
+                      name=""
+                      value={order.status}
+                      id=""
+                      onChange={(e) =>
+                        handleStatusChange(order._id, e.target.value)
+                      }
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5"
+                    >
+                      <option value="Processing">Processing</option>
+                      <option value="Shipped">Shipped</option>
+                      <option value="Delivered">Delivered</option>
+                      <option value="Cancelled">Cancelled</option>
+                    </select>
+                  </td>
+                  <td className="p-4">
+                    <button
+                      onClick={() => handleStatusChange(order._id, "Delivered")}
+                      className="cursor-pointer bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded"
+                    >
+                      Mark as Delivered
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td className="p-4 text-center text-gray-500 " colSpan={5}>
+                  No orders found.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default OrderManagement;
