063613974e1dddcc3f4e20df21d40b19ed0d0f56
mhjim <mhjim.info@gmail.com>
2025-04-05 19:32:25 +0600

    working on checkout finalizing

--- backend/routes/checkoutRoutes.js
+++ backend/routes/checkoutRoutes.js
@@ -5,6 +5,8 @@ const Product = require("../models/Product");
 const Order = require("../models/Order");
 const { protect, admin } = require("../middleware/authMiddleware");
 
+const router = express.Router();
+
 //@route POST /api/checkout
 //@desc Create a new checkout session
 //@access Private
@@ -25,7 +27,74 @@ router.post("/", protect, async (req, res) => {
       shippingAddress,
       PaymentMethod,
       totalPrice,
-      payment,
+      paymentStatus,
+      isPaid: false,
     });
-  } catch (error) {}
+    console.log(`Checkout created for user: ${req.user._id}`);
+    res.status(201).json(newCheckout);
+  } catch (error) {
+    console.error("Error creating checkout session:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//@route PUT /api/checkout/:id/pay
+//@desc Update checkout to mark as paid after successfull payment
+//@access Private
+router.put("/:id/pay", protect, async (req, res) => {
+  const { checkoutItems, shippingAddress, PaymentMethod, totalPrice } =
+    req.body;
+
+  try {
+    const checkout = await Checkout.findById(req.params.id);
+
+    if (!checkout) {
+      return res.status(404).json({ message: "Checkout not found" });
+    }
+
+    if (paymentStatus === "paid") {
+      checkout.isPaid = true;
+      checkout.paymentStatus = paymentStatus;
+      checkout.paymentDetails = paymentDetails;
+      checkout.paidAt = Date.now();
+
+      await checkout.save();
+
+      res.status(200).json(checkout);
+    } else {
+      res.status(400).json({ message: "Invalid payment status" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+//@route POST /api/checkout/:id/finalize
+//@desc Finalize checkout and convert to an order after payment confirmation
+//@access Private
+router.post("/:id/finalize", protect, async (req, res) => {
+  try {
+    const checkout = await Checkout.findById(req.params.id);
+
+    if (!checkout) {
+      return res.status(404).json({ message: "Checkout not found" });
+    }
+
+    if (checkout.isPaid && !checkout.isFinalized) {
+      //Create final order based on the checkout details
+      const finalOrder = await Order.create({
+        user: checkout.user.
+      })
+
+      await checkout.save();
+
+      res.status(200).json(checkout);
+    } else {
+      res.status(400).json({ message: "Invalid payment status" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
