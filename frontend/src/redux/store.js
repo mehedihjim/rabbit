0792c6db6cfb678190db5943c273e72884fc44bf
mhjim <mhjim.info@gmail.com>
2025-04-12 00:37:36 +0600

    checkoutSLice done

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import productReducer from "./slice/productsSlice";
 import cartReducer from "./slice/cartSlice";
+import checkoutReducer from "./slice/checkoutSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     products: productReducer,
     cart: cartReducer,
+    checkout: checkoutReducer,
   },
 });
 
