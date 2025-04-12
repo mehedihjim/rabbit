70dcc596c3502cadbe3734a05c0cc07ea6dc96ed
mhjim <mhjim.info@gmail.com>
2025-04-12 22:11:45 +0600

    SearchBar func done

--- frontend/src/components/Common/SearchBar.jsx
+++ frontend/src/components/Common/SearchBar.jsx
@@ -1,50 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { HiMagnifyingGlass, HiMiniXMark } from "react-icons/hi2";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router";
+import {
+  fetchProductsByFilters,
+  setFilters,
+} from "./../../redux/slice/productsSlice";
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [searchTerm, setSearchTerm] = useState('')
-    const [isOpen, setIsOpen] = useState(false)
+  const handleSearchToggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const handleSearchToggle = () => {
-        setIsOpen(!isOpen)
-    }
+  const handleSearch = (e) => {
+    e.preventDefault();
+    dispatch(setFilters({ search: searchTerm }));
+    dispatch(fetchProductsByFilters({ search: searchTerm }));
+    navigate(`/collections/all?search=${searchTerm}`);
+    setIsOpen(false);
+  };
 
-    const handleSearch = (e) => {
-        e.preventDefault()
-        console.log("Search Term:", searchTerm)
-        setIsOpen(false)
-    }
+  return (
+    <div
+      className={`flex items-center justify-center w-full transition-all duration-300 ${
+        isOpen ? "absolute w-full top-0 left-0 bg-white h-24 z-50" : "w-auto"
+      }`}
+    >
+      {isOpen ? (
+        <form
+          onSubmit={handleSearch}
+          className="relative flex items-center justify-center w-full"
+        >
+          <div className="relative w-1/2">
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none placeholder:text-gray-700 w-full"
+            />
+            {/* Search Button */}
+            <button
+              type="submit"
+              className="absolute right-2 top-1/2 -translate-y-1/2 text-gray-600 hover:text-gray-800 cursor-pointer"
+            >
+              <HiMagnifyingGlass className="w-6 h-6" />
+            </button>
+          </div>
+          <button type="button" className="ml-6" onClick={handleSearchToggle}>
+            <HiMiniXMark className="h-6 w-6 cursor-pointer" />
+          </button>
+        </form>
+      ) : (
+        <button onClick={handleSearchToggle} className="cursor-pointer">
+          <HiMagnifyingGlass className="w-6 h-6" />
+        </button>
+      )}
+    </div>
+  );
+};
 
-    return (
-        <div className={`flex items-center justify-center w-full transition-all duration-300 ${isOpen ? "absolute w-full top-0 left-0 bg-white h-24 z-50" : "w-auto"}`} >
-            {
-                isOpen ?
-                    (<form onSubmit={handleSearch} className='relative flex items-center justify-center w-full' >
-                        <div className="relative w-1/2">
-                            <input type="text"
-                                placeholder='Search'
-                                value={searchTerm}
-                                onChange={(e) => setSearchTerm(e.target.value)}
-                                className='bg-gray-100 px-4 py-2 pl-2 pr-12 rounded-lg focus:outline-none placeholder:text-gray-700 w-full'
-                            />
-                            {/* Search Button */}
-                            <button type='submit' className='absolute right-2 top-1/2 -translate-y-1/2 text-gray-600 hover:text-gray-800 cursor-pointer'>
-                                <HiMagnifyingGlass className='w-6 h-6' />
-                            </button>
-                        </div>
-                        <button type='button' className='ml-6' onClick={handleSearchToggle}>
-                            <HiMiniXMark className='h-6 w-6 cursor-pointer' />
-                        </button>
-                    </form >
-
-                    ) : (
-
-                        <button onClick={handleSearchToggle} className='cursor-pointer'>
-                            <HiMagnifyingGlass className='w-6 h-6' />
-                        </button>)}
-        </div >
-    )
-}
-
-export default SearchBar
+export default SearchBar;
